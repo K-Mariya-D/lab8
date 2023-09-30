5c86df6df8c391fa3b1775c3b897b21c59f98e69
Мария Корчагина <kmd2oo619@gmail.com>
2023-09-30 09:33:22 +0300

    Определяет високосность года

---
+++ time.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var n:= ReadInteger('Введите год:');
+  if (n mod 4 = 0) then
+    if (n mod 100 = 0) and (n mod 400 <> 0) then
+      Print('Год не високосный')
+    else Print('Год високосный')
+  else 
+    Print('Год не високосный');
+end.
