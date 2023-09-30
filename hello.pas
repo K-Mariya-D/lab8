3a816b03dc175ddc6eee67ace193f918e01a0b5f
Мария Корчагина <kmd2oo619@gmail.com>
2023-09-30 09:24:05 +0300

    Какое-то сообщение

--- hello.pas
+++ hello.pas
@@ -1,3 +1,9 @@
 ﻿begin
-  Print('Hell0 world!');
+  var n:= ReadInteger('Введите, который сейчас час:');
+  case n of
+    4..10: Print('Доброе утро, мир!');
+    11..16: Print('Добрый день, мир!');
+    17..22: Print('Добрый вечер, мир!');
+    else Print('Доброй ночи, мир!');
+  end;
 end.
